d9b8068a8deafa3070c618cadddc4f6deb99e6a5
ricardocollado <ricardocolladorothschild@gmail.com>
2023-12-09 01:13:53 -0400

    create class Product

---
+++ Productos.ts
@@ -0,0 +1,14 @@
+
+class Productos{
+    private product_name:string;
+    private product_price:string;
+
+        constructor(){
+            this.product_name = 'all';
+            this.product_price = "0";
+        }
+
+        allProductsReturn(){
+            
+        }
+}
--- script/Productos.js
+++ script/Productos.js
@@ -1,1 +1,9 @@
 "use strict";
+class Productos {
+    constructor() {
+        this.product_name = 'all';
+        this.product_price = "0";
+    }
+    allProductsReturn() {
+    }
+}
