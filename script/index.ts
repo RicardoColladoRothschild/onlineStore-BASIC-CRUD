ccdee6d411156eb503874842c1553882e907e988
ricardocollado <ricardocolladorothschild@gmail.com>
2023-12-30 00:07:33 -0400

    prior

---
+++ script/index.ts
@@ -0,0 +1,20 @@
+const btn_acceder = document.querySelector('#acceder');
+
+
+
+async function requestAccess(){
+    const response = await fetch('http://localhost:3000/obtener-datos');
+    if(!response.ok){
+        console.log(response.status);
+        throw new Error('Error en la socilitud: ' + response.status);
+    }
+
+    const datos = await response.json();
+    console.log(datos);
+    /*const d = JSON.stringify(datos, null, 2);
+    console.log(d);*/
+}
+btn_acceder?.addEventListener('click', ()=>{
+    requestAccess();
+    console.log('testing dom manipulation');
+});
