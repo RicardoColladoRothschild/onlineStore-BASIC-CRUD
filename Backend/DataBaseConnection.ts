dce62e4bb5b591bcbfed89a48bd6164476dca356
ricardocollado <ricardocolladorothschild@gmail.com>
2024-01-02 00:47:42 -0400

    Create DataBaseConenction class 1) Class has a query to reiceve 2) Class DataBaseConnection has acreateConnection method that will stablish connection to the database

---
+++ Backend/DataBaseConnection.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import * as sqlite3 from 'sqlite3';
+export class DataBaseConnection{
+    private _query:string;
+    private dbPath:string = path.join(__dirname,'../database/Proyecto_Practica_CRUD_dataBase.db');
+
+        constructor(query:string){
+            this._query = query;
+            this.createConnection();
+        }
+
+        async createConnection(){
+            return new Promise((resolve, reject)=>{
+                const db = new sqlite3.Database(this.dbPath, sqlite3.OPEN_READONLY, (err)=>{
+                    if(err){
+                        console.error(err.message);
+                        reject(err);
+                    }else{
+                        resolve('Connection to data base was succefully..')
+                    }
+                });
+            });
+        }
+}
